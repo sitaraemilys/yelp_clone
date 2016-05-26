f06608100036fd068a9572baf8f8259a9c3069ba
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-26 12:44:44 +0100

    Tests failing for one review per restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,16 @@ class ReviewsController < ApplicationController
 	def new
 		@restaurant = Restaurant.find(params[:restaurant_id])
 		@review = Review.new
+
+		if @review.save
+			redirect_to restaurants_path
+		else
+			if @review.errors[:user]
+				redirect_to restaurants_path alert: 'You have already reviewed this restaurant'
+			else
+				render :new
+			end
+		end
 	end
 
 	def create
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ActiveRecord::Base
   validates :rating, inclusion: (1..5)
   belongs_to :user
   belongs_to :restaurant
+
+  validates :user, uniqueness: { scope: :restaurant, message: "has reviewed this restaurant already" }
 end
