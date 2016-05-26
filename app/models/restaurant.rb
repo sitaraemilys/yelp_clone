95e3813980c864b08b897a08ec2597cff3c90c2a
sitypop <sityemilyshah@gmail.com>
2016-05-26 16:04:28 +0100

    build_review method in the making

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,8 @@
 class Restaurant < ActiveRecord::Base
-	has_many :reviews, dependent: :destroy
+	has_many :reviews,
+		-> { extending WithUserAssociationExtension },
+	dependent: :destroy
+
 	validates :name, length: {minimum: 3}, uniqueness: true
 
 	belongs_to :user
